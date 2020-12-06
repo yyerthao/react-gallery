7ebfe2107b8ad56d36ed99be110ee99637b7a375
Yer Thao <maivyerthao@gmail.com>
2020-12-05 23:09:03 -0600

    Update photo descriptions

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/duluth1.jpg', title: 'Sun gals', description: 'Big Lake, MN.', likes: 0 },
-    { id: 2, path: 'images/duluth2.jpg', title: 'Swings', description: 'Hanging at the park', likes: 0 },
-    { id: 3, path: 'images/duluth3.jpg', title: 'Swings', description: 'Hanging at the park', likes: 0 },
-    { id: 4, path: 'images/duluth5.jpg', title: 'Swings', description: 'Hanging at the park', likes: 0 }
+    { id: 1, path: 'images/duluth1.jpg', title: 'Duluth', description: '1', likes: 0 },
+    { id: 2, path: 'images/duluth2.jpg', title: 'Duluth', description: 'A', likes: 0 },
+    { id: 3, path: 'images/duluth3.jpg', title: 'Duluth', description: 'B', likes: 0 },
+    { id: 4, path: 'images/duluth5.jpg', title: 'Duluth', description: 'C', likes: 0 }
 ];
 
 module.exports = galleryItems;
