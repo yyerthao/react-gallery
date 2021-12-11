6c4cdc07525a436ba5fa5ab94a1e9fb6195caa7b
Yer Thao <maivyerthao@gmail.com>
2021-12-11 16:23:04 -0600

    Added material ui to GalleryItem.js

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
-import './GalleryItem.css';
 import axios from 'axios';
 
+
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid"
+
+const styles = {
+  card: {
+    maxWidth: 500,
+  },
+  media: {
+    height: 200,
+  },
+};
 
 
 class GalleryItem extends Component {
@@ -42,13 +61,53 @@ class GalleryItem extends Component {
       });
   };
 
-  render() {
+  render(props) {
+    const { classes } = this.props;
     return (
       <>
         <div className="outer-div">
           <div onClick={this.toggleImage}>
             {this.state.displayPic ? (
-              <img src={this.props.image.path} alt="Yer's Photos"></img>
+              // <img src={this.props.image.path} alt="Yer's Photos"></img>
+              <div>
+               <Grid
+                        container
+                        spacing={4}
+                        className={classes.gridContainer}
+                        justify="center"
+                      >
+                        <Grid item xs={12} sm={6} md={4}>
+                          <Card className={classes.card}>
+                              <CardActionArea>
+                                  <CardMedia
+                                  className={classes.media}
+                                  image={this.props.image.path}
+                                  alt="Yer's Photos"
+                                  />
+                                  <CardContent>
+                                  <Typography gutterBottom variant="h5" component="h2">
+                                      Destination Photo
+                                  </Typography>
+                                  <Typography component="p">
+                                      TEXT WILL GO HERE 
+                                  </Typography>
+                                  </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                  <Button size="small" color="primary" onClick={this.handleLike}>
+                                  Like
+                                  </Button>
+                                  <Button size="small" color="primary" onClick={this.handleUnlike}>
+                                  Unlike
+                                  </Button>
+                              </CardActions>
+                              </Card>
+                        </Grid>
+                  </Grid>
+                  
+              </div>
+
+
             ) : (
               <p className="img-btn">{this.props.image.description}</p>
             )}
@@ -60,8 +119,6 @@ class GalleryItem extends Component {
                 : `${this.props.image.likes} people like this`}
             </p>
           </div>
-          <button onClick={this.handleLike}>Like</button>
-          <button onClick={this.handleUnlike}>Unlike</button>
         </div>
       </>
     );
@@ -69,7 +126,12 @@ class GalleryItem extends Component {
 }
 
 
-export default GalleryItem;
+GalleryItem.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(GalleryItem);
+
 
 
 // get like button to connect to put route, counter already done
